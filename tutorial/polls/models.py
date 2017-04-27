f50f52d5048ab018181abfd77e0b3400a8d68dfa
Willian Braga da Silva <willian.to@corp.globo.com>
2017-04-27 19:31:55 -0300

    UDPDATE: Updating tutorial

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -20,3 +20,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)

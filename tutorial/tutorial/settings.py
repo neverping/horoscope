8b110a06a5d37dfa9afe515ce4d97e8088cab063
Willian Braga da Silva <neverping@gmail.com>
2017-04-29 21:38:32 -0300

    UPDATE: We should set DEBUG to False.

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nf%a#t=4(p@jiu^w&b62odel4u-*8erhs_^+0_s^a7yjv4p4ey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
